6f300097260e71565be6599a14eacab83dc5f4ef
Cesar <53570304+BoBomann18@users.noreply.github.com>
2020-07-14 15:29:30 +0200

    Add files via upload
    Initial commit

---
+++ cmus/lib.pl
@@ -0,0 +1,5 @@
+/home/cesar/Musik/Demons.mp3
+/home/cesar/Musik/Piece Of Your Heart.mp3
+/home/cesar/Musik/Pop Out.mp3
+/home/cesar/Musik/Closer.mp3
+/home/cesar/Musik/Sick Boy.mp3
---
+++ cmus/playlist.pl
@@ -0,0 +1,2 @@
+/home/cesar/Musik/Sick Boy.mp3
+/home/cesar/Musik/Closer.mp3
